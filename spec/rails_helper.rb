55ff185189eaaf700f97d81977b853075128c8bd
Yomi Colledge <baphled@boodah.net>
2026-05-14 16:54:56 +0100

    refactor: clean up corrupted rails_helper.rb
    Removed 9+ duplicated comment blocks, reducing file from 163 to 62 lines.
    Added proper support file loading and cleaned configuration.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -24,107 +23,8 @@ require 'rspec/rails'
 # penalty is bearable. For convenience, you may want to remove this line in
 # your own test suite.
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec` by setting
-# `--pattern` glob to not match files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with `_spec.rb`. To configure this
-# see the RSpec configuration docs.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec` by setting
-# `--pattern` glob to not matching files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with `_spec.rb`. To configure this
-# see the RSpec configuration docs.
-
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the booting time of your test suite. Because you only need to
-# boot the test suite once during your development workflow, the performance
-# penalty is bearable. For convenience, you may want to remove this line in
-# your own test suite.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This for convenience, you may want to remove this
-# line in your own test suite.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec specs by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec` by setting
-# `--pattern` glob to not match files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with `_spec.rb`. To configure this
-# see the RSpec configuration docs.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec` by setting
-# `--pattern` glob to not match files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with `_spec.rb`. To configure this
-# see the RSpec configuration docs.
-
-# Begin RSpec configuration
-
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the booting time of your test suite. Because you only need to
-# boot the test suite once during your development workflow, the performance
-# penalty is bearable. For convenience, you may want to remove this line in
-# your own test suite.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the engineer or in ~/.rspec, .rspec or `.rspec` by setting
-# `--pattern` glob to not match files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with _spec.rb`. To configure this
-# see the RSpec configuration docs.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec` by allowing
-# `--pattern` glob to not match files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with _spec.rb`. To configure this
-# see the RSpec configuration docs.
-
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the booting time of your test suite. Because you only need to
-# boot the test suite once during your development workflow, the performance
-# penalty is bearable. For convenience, you persistence to remove this line in
-# your own test suite.
-
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in specs in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the specs to be
-# run twice. It is recommended that you do not name files matching this glob to
-# end with _spec.rb. You can configure this glob with the --pattern
-# option on the command line or in ~/.rspec, .rspec or `.rspec` by setting
-# `--pattern` glob to not match files ending in `_spec.rb` while also not
-# requiring the files in spec/support to end with _spec.rb`. To configure this
-# see the RSpec configuration docs.
+# Load support files
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Begin RSpec configuration
 
